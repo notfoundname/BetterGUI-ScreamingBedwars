ec9f3d6c5bda25899938ec674609c344e339fa37
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-03-12 11:35:57 +0800

    Create AutojoinCommand.java

---
+++ src/scbw/commands/AutojoinCommand.java
@@ -0,0 +1,31 @@
+package scbw.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.screamingsandals.bedwars.Main;
+import org.screamingsandals.bedwars.api.game.Game;
+import me.hsgamer.bettergui.lib.taskchain.TaskChain;
+import me.hsgamer.bettergui.object.Command;
+
+public class AutojoinCommand extends Command {
+
+	public AutojoinCommand(String string) {
+		super(string);
+	}
+
+	@Override
+	public void addToTaskChain(Player player, TaskChain<?> taskChain) {
+		taskChain.sync(() -> autoJoin(player));
+	}
+	public void autoJoin(Player player) {
+		if (Main.isPlayerInGame(player)) {
+            player.sendMessage(ChatColor.RED + "You are already playing a game!");
+        }
+        Game game = Main.getInstance().getFirstWaitingGame();
+        if (game == null) {
+            player.sendMessage(ChatColor.RED + "There is no empty game.");
+        } else {
+            game.joinToGame(player);
+        }
+	}
+}
