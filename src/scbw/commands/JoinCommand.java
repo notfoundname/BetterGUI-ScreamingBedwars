33fc5a349f0da4db3577edeafc003aa869af08b3
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-03-12 11:36:48 +0800

    Create JoinCommand.java

---
+++ src/scbw/commands/JoinCommand.java
@@ -0,0 +1,32 @@
+package scbw.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.screamingsandals.bedwars.Main;
+import me.hsgamer.bettergui.lib.taskchain.TaskChain;
+import me.hsgamer.bettergui.object.Command;
+
+public class JoinCommand extends Command {
+
+	public JoinCommand(String string) {
+		super(string);
+	}
+
+	@Override
+	public void addToTaskChain(Player player, TaskChain<?> taskChain) {
+		String parsed = getParsedCommand(player);
+		taskChain.sync(() -> joinGame(player, parsed));
+	}
+	
+	public void joinGame(Player p, String gameName) {
+		if (Main.isPlayerInGame(p)) {
+            p.sendMessage(ChatColor.RED + "You are already playing some game!");
+        }
+        if (Main.isGameExists(gameName)) {
+        	Main.getGame(gameName).joinToGame(p);
+        } else {
+            p.sendMessage(ChatColor.RED + "Game does not exist!");
+        }
+    }
+}
+
